324c23d4ab0c692a477cfa7751c17d6b49b1de41
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-02 14:23:59 +0530

    jenkins

---
+++ vars/mainci.groovy
@@ -0,0 +1,27 @@
+def call() {
+    node { label 'workstation'}
+    if (env.cibuild == "java") {
+
+        stage('Build') {
+            sh 'mvn package'
+        }
+        stage('unit tests') {
+            echo 'unit tests'
+
+        }
+        stage('Code Analysis') {
+            sh 'sudo sonar-scanner -Dsonar.host.url=http://172.31.68.239:9000 -Dsonar.login=admin -Dsonar.password=DevOps321 -Dsonar.projectKey=shipping'
+        }
+
+        stage('Security Scans') {
+            echo 'Security Scans'
+        }
+        if (env.TAG_NAME ==~ ".*") {
+            stage('Publish a Artifact') {
+                echo 'Publish a Artifact'
+            }
+        }
+    }
+}
+
+
