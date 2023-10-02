7ec4506af545eed9d07d462878dc65178583ef0e
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-02 19:09:17 +0530

    jenkins

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,17 +1,23 @@
 def call() {
-    node { label 'workstation'}
-    if (env.cibuild == "java") {
+    node('workstation') {
 
-        stage('Build') {
-            sh 'mvn package'
+        stage('code checkout') {
+            sh 'env'
+            sh 'find . | grep "./" | xrgs rm -rf'
+            git branch: env.BRANCH_NAME , url: 'https://github.com/Rajesh-2406/frontend'
         }
-    }
+        if (env.cibuild == "java") {
+
+            stage('Build') {
+                sh 'mvn package'
+            }
+        }
         stage('unit tests') {
             echo 'unit tests'
 
         }
         stage('Code Analysis') {
-           // sh 'sudo sonar-scanner -Dsonar.host.url=http://172.31.68.239:9000 -Dsonar.login=admin -Dsonar.password=DevOps321 -Dsonar.projectKey=shipping'
+            // sh 'sudo sonar-scanner -Dsonar.host.url=http://172.31.68.239:9000 -Dsonar.login=admin -Dsonar.password=DevOps321 -Dsonar.projectKey=shipping'
         }
 
         stage('Security Scans') {
@@ -23,6 +29,6 @@ def call() {
             }
         }
     }
-
+}
 
 
