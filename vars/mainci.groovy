d20f9f2d6c1d6152c6b4159b7b7c93366edb732f
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-07 14:02:19 +0530

    artifact

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -12,12 +12,7 @@ def call() {
 
             checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: "https://github.com/Rajesh-2406/${env.component}"]], branches: [[name: gitbrname]]],poll: false
         }
-        if (env.cibuild == "java") {
 
-            stage('Build') {
-              sh 'mvn package'
-            }
-        }
         stage('unit tests') {
             echo 'unit tests'
 
@@ -31,6 +26,13 @@ def call() {
         }
         if (env.TAG_NAME ==~ ".*") {
             stage('Publish a Artifact') {
+                if (env.cibuild == "java") {
+
+                    stage('Build') {
+                        sh 'mv target/${component}-1.0.jar ${component}.jar'
+                        sh 'rm -rf pom.xml src target'
+                    }
+                }
                 sh 'rm -f Jenkinsfile'
                 sh 'echo ${TAG_NAME} >VERSION'
                 sh 'zip -r ${component}-${TAG_NAME}.zip *'
